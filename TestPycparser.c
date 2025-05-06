94a2cab1cdf44503664a054b25ccd31599e47233
xkey3960 <71362705+xkey3960@users.noreply.github.com>
2025-05-07 00:07:49 +0800

    完整支持 赋值语句 和 循环中使用复合表达式 - 验证代码

--- TestPycparser.c
+++ TestPycparser.c
@@ -1,17 +1,11 @@
-void printArray()
+int main(int argc, char *argv[])
 {
     int iLoop = 0;
     int iTest = 0;
-
-    // for (iLoop = 0; iLoop<10; (void)({iLoop++;iTest=iLoop;}))
-    // {
-    //     iLoop;
-    // }
-}
-
-int main(int argc, char *argv[])
-{
-    int x = ({int y = 1; y+1; });
-    printArray();
+    int x = ({int y = 1; y++;});
+    for (iLoop = 0; iLoop<10; (void)({iLoop++;iTest=iLoop;}))
+    {
+        iLoop;
+    }
     return 0;
 }
